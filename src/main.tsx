47f5bdd2a3a73faa926223e405a709544404c0b4
MaciejUlasiuk <filip123xd@o2.pl>
2023-02-12 23:05:35 +0100

    updt

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-
+import { Provider } from 'react-redux'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { store } from './app/store'
+import MainPage from './MainPage'
+import LoginPage from './pages/LoginPage'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainPage/>} />
+  )
+)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+        <RouterProvider router={router}/>
+  </React.StrictMode>
+  </Provider>
 )
