49d7a2fcff6a797135fa5d08ecb5f72740b52ea9
MaciejUlasiuk <filip123xd@o2.pl>
2023-02-19 13:50:57 +0100

    router and base url change

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import { store } from './app/store'
 import MainPage from './MainPage'
 import LoginPage from './pages/LoginPage'
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <>
     <Route path='/' element={<LoginPage/>}/>
