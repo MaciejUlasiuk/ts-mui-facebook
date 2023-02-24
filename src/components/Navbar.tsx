4ceb50ecdd414b09ae09be538ffd20c599512892
MaciejUlasiuk <filip123xd@o2.pl>
2023-02-24 22:21:50 +0100

    updt

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,12 +7,20 @@ import {
   InputBase,
   Menu,
   MenuItem,
+  PaletteMode,
   styled,
   Toolbar,
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { redirect, useNavigate } from "react-router-dom";
+import { RootState } from "../app/store";
+import { logoutUser } from "../features/userSlice";
+import {ListItem, ListItemButton, ListItemIcon, Switch} from "@mui/material"
+import { ModeNight } from "@mui/icons-material";
+import { setMode } from "../features/modeSlice";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -27,12 +35,12 @@ const Search = styled("div")(({ theme }) => ({
 }));
 
 const Icons = styled(Box)(({ theme }) => ({
-  display: "none",
+  display: "flex",
   alignItems: "center",
   gap: "20px",
-  [theme.breakpoints.up("sm")]: {
-    display: "flex",
-  },
+  // [theme.breakpoints.up("sm")]: {
+  //   display: "flex",
+  // },
 }));
 
 const UserBox = styled(Box)(({ theme }) => ({
@@ -44,24 +52,34 @@ const UserBox = styled(Box)(({ theme }) => ({
   },
 }));
 const Navbar = () => {
+  const user = useSelector((state: RootState)=>state.userSlice.user)
+  const mode = useSelector((state: RootState)=>state.modeSlice.mode)
   const [open, setOpen] = useState(false);
-  
+  const dispatch = useDispatch()
   const navigate = useNavigate()
   return (
-    <AppBar position="sticky">
-      <StyledToolbar>
-        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+    <AppBar position="sticky" sx={{ overflow: 'hidden'}}>
+      <StyledToolbar >
+        <Typography variant="h6"  sx={{ display: { xs: "none", sm: "block" } }}>
           Facebook Clone
         </Typography>
-        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
-        <Search>
+        <Pets sx={{ display: { xs: "flex", sm: "none" }, alignItems: 'flex-start' }} />
+        {user ? <Search>
           <InputBase placeholder="search..." />
-        </Search>
-        <Icons>
-          <Badge badgeContent={4} color="error">
+        </Search> : null}
+       { user ? null : <ListItem disablePadding  sx={{width: {sm: '100px',  lg: '30%'}} }>
+              <ListItemButton >
+                <ListItemIcon>
+                  <ModeNight />
+                </ListItemIcon>
+                <Switch onChange={()=>{mode === 'dark' ? dispatch(setMode('light')) : dispatch(setMode('dark')) }}/>
+              </ListItemButton>
+            </ListItem> }
+       { user ? <Icons>
+          <Badge badgeContent={4} color="error" sx={{display: {xs: 'none', md: 'flex'}}}>
             <Mail sx={{cursor: 'pointer'}}/>
           </Badge>
-          <Badge badgeContent={2} color="error">
+          <Badge badgeContent={2} color="error" sx={{display: {xs: 'none', md: 'flex'}}}>
             <Notifications sx={{cursor: 'pointer'}}/>
           </Badge>
           <Avatar
@@ -69,13 +87,13 @@ const Navbar = () => {
             src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
             onClick={(e) => setOpen(true)}
           />
-        </Icons>
-        <UserBox onClick={(e) => setOpen(true)} >
+        </Icons> : null}
+        {/* { user ? null :<UserBox onClick={(e) => setOpen(true)} >
           <Avatar
             src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           />
           <Typography variant="subtitle1">John</Typography>
-        </UserBox>
+        </UserBox>} */}
       </StyledToolbar>
       <Menu
         id="demo-positioned-menu"
@@ -93,7 +111,7 @@ const Navbar = () => {
       >
         <MenuItem >Profile</MenuItem>
         <MenuItem >My account</MenuItem>
-        <MenuItem onClick={()=>navigate('/')}>Logout</MenuItem>
+        <MenuItem onClick={()=>dispatch(logoutUser(false))}>Logout</MenuItem>
       </Menu>
     </AppBar>
   );
