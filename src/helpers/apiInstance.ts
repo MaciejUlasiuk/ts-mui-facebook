202fd4fe5c2a74302673b119843997db39a4586d
MaciejUlasiuk <filip123xd@o2.pl>
2023-03-03 22:22:27 +0100

    axios helper

--- src/helpers/apiInstance.ts
+++ src/helpers/apiInstance.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: 'https://dummyapi.io/data/v1/',
+    responseType: 'json',
     headers: {
         'app-id': '63f63630896fbb0b704168bd',
     }
